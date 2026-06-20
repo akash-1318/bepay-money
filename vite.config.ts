70445d6cdc858e90c24288b947fb55617f400b09
Akash Sharma <akash_18@192.168.1.9>
2026-06-20 13:43:25 +0530

    feat: added payment history page

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "react": "preact/compat",
+      "react-dom": "preact/compat",
+      "react-dom/test-utils": "preact/compat/test-utils",
+      "react/jsx-runtime": "preact/jsx-runtime"
     },
   },
 })
